0fd1b3cae82d891cb1e9a469886090e7fce8c560
maceyhartmann <maceyhartmann@icloud.com>
2026-05-21 14:54:37 -0700

    adding research question visualizations and exploratory visualizations

---
+++ Untitled.R
@@ -0,0 +1,19 @@
+---
+  title: "Salt Marsh Vegetation at NCOS"
+subtitle: "Species composition and native/non-native cover across survey years"
+author: "Macey Hartmann and Erick Morales Oyola"
+date: 5/18/2026
+format: 
+  pdf:
+  include-in-header: 
+  text: |
+  \usepackage{fvextra}
+\DefineVerbatimEnvironment{Highlighting}{Verbatim}{breaklines,commandchars=\\\{\}}
+\DefineVerbatimEnvironment{OutputCode}{Verbatim}{breaklines,commandchars=\\\{\}}
+editor: source
+execute: 
+  message: false
+warning: false
+---
+  
+# Guiding questions
